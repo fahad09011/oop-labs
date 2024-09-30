1b2baea8d5657bfc64f915b0f1ed35206e262bb0
fahad0901 <fahadimran038@gmail.com>
2024-10-01 00:33:34 +0100

    modified:   OopLab2/Ooplab2Pb/rectangle.java 	new file:   OopLab2/Ooplab2Pb/rectangleshape.java

--- OopLab2/Ooplab2Pb/rectangle.java
+++ OopLab2/Ooplab2Pb/rectangle.java
@@ -3,49 +3,79 @@ public class rectangle {
     private double width;
 
     public rectangle() {
-this.length=1.0;
-this.width=1.0;
+        this.length = 1.0;
+        this.width = 1.0;
     }
 
-    public double  getlength() {
+    public double getlength() {
         return length;
     }
 
-    public void setlength(double  length) {
+    public void setlength(double length) {
         if (length > 0 && length <= 40) {
             this.length = length;
+        } else {
+            System.out.println("length must be greater than 0 and less than equal to 40");
         }
-        else{
-            System.out.println("length must be greater than 0 and less than equal to 40"); }
 
     }
 
-    public double  getwidth() {
+    public double getwidth() {
         return width;
     }
 
-    public void setwidth(double  width) {
+    public void setwidth(double width) {
 
         if (width > 0 && width <= 40) {
             this.width = width;
-        }
-        else{
+        } else {
             System.out.println("width must be greater than 0 and less than equal to 40");
         }
 
     }
+
     // question 2 getarea and perameter
-    public double  getarea(){
-        return length*width;
+    public double getarea() {
+        return length * width;
     }
 
-    public  double getperimeter(){
-        return 2*(length + width);
+    public double getperimeter() {
+        return 2 * (length + width);
+    }
+
+    // question 3 print rectangle with ***
+    public void printrectangle() {
+        int intlength = (int) length;
+        int intwidth = (int) width;
+
+        // to print top row
+        for (int i = 0; i < intwidth; i++) {
+            System.out.println("*"); //.....
+        }
+        System.out.println();
+
+        // to print left right sides
+        for(int i=0 ; i<intlength-2; i++){
+            System.out.println("*"); //left.....
+            for(int j=0 ; j<intwidth-2; j++){
+                System.out.println( " "); //center.....
+            }
+            System.out.println("*"); //right
+        }
+
+        // to print botton row
+        if (intlength > 1) {
+            for (int i = 0; i < intwidth; i++) {
+                System.out.println("*"); //......
+            }
+            System.out.println();
+        }
+        
     }
 
     @Override
-    public String toString(){
-        return "length = "+length +" width = "+width;
-    } 
+    public String toString() {
+        return "length = " + length + " width = " + width;
+    }
 
 }
---
+++ OopLab2/Ooplab2Pb/rectangleshape.java
@@ -0,0 +1,13 @@
+public class rectangleshape {
+    public static void main(String[] args) {
+        rectangle rect1 = new rectangle();
+
+        // Setting values for length and width
+        rect1.setlength(12);
+        rect1.setwidth(6);
+
+        // Printing the rectangle
+        System.out.println("Rectangle 1:");
+        rect1.printrectangle();
+    }
+}
