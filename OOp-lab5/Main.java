1142b91ba95c3ece4a2e417e0c6dc806787acd3e
fahad09011 <fahadimran038@gmail.com>
2024-11-10 14:31:45 +0000

    modified:   OOp-lab5/Main.java

--- OOp-lab5/Main.java
+++ OOp-lab5/Main.java
@@ -1,9 +1,23 @@
 
 public class Main {
     public static void main(String[] args) {
+        //constructor values
+        System.out.println(" with default constructor values ");
         Point newpoint = new Point(10,20);
         Circle newcircle = new Circle(45, 8, 7);
         System.out.println("Point details : "+newpoint);
-        System.out.println("circle details : "+newcircle);
+        System.out.println("circle details : "+newcircle +"\n");
+
+// with change values
+        System.out.println("with change values");
+        newpoint.setX(12);
+        newpoint.setY(11);
+
+        newcircle.setX(12);
+        newcircle.setY(11);
+        newcircle.setRadius(87);
+
+    System.out.println( "change Point values : "+newpoint );
+    System.out.println( "change Circle values : "+newcircle );
     }
 }
