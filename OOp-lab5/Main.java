56f6ec4be91b51d72387e5a2b5feec6dc19e0080
fahad09011 <fahadimran038@gmail.com>
2024-11-10 14:20:25 +0000

    modified:   OOp-lab5/Main.java 	modified:   OOp-lab5/Point.java

--- OOp-lab5/Main.java
+++ OOp-lab5/Main.java
@@ -2,7 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         Point newpoint = new Point(10,20);
-
+        Circle newcircle = new Circle(45, 8, 7);
         System.out.println("Point details : "+newpoint);
+        System.out.println("circle details : "+newcircle);
     }
 }
--- OOp-lab5/Point.java
+++ OOp-lab5/Point.java
@@ -31,3 +31,31 @@ public class Point {
         
     
 }
+
+class Circle extends Point{
+    
+
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public String toString(){
+        String result = "super class : "+super.toString()+" radius : "+radius;
+
+        return result;
+    }
+    
+
+}
